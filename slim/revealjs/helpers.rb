cb2df0a68c6e194d3e5e63952896f26f0f64030e
Olivier Bilodeau <olivier@bottomlesspit.org>
2014-09-29 21:27:54 -0400

    failed attempt at having a more resistant to mistakes revealjs config

--- slim/revealjs/helpers.rb
+++ slim/revealjs/helpers.rb
@@ -3,6 +3,15 @@
 # (unless someone can show me how to include them in the evaluation context).
 # You can change the namespace to whatever you want.
 module Helpers
-  #def self.a_helper_function
-  #end
+  # Returns a javascript true or false string depending on the content stored
+  # in the given attribute.
+  def self.attr_jsbool(attribute_name, default)
+
+    value = attr(attribute_name, default);
+    if value
+      return 'true';
+    else
+      return default;
+    end
+  end
 end
